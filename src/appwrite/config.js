c59265d82e055edfd970d86c6d3a994e263b9ba2
Syed Tasavour <syedtasavour86@gmail.com>
2025-02-19 17:24:20 +0530

    add methods for post management and file handling in Appwrite service

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -58,7 +58,70 @@ export class Service {
     } catch (error) {
       return false
     }
+  } 
+  async getPost(slug) {
+    try {
+      return  await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      )
+      
+    } catch (error) {
+      console.log("Appwrite service :: getPost :: error");
+      return false
+    }
   }
+  async listPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return  await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+      queries,
+
+      )
+      
+    } catch (error) {
+      console.log("Appwrite service :: listPost :: error");
+      return false
+    }
+  }
+
+  async uplaodFile(file){
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      )
+      
+    } catch (error) {
+      console.log("Appwrite service :: fileUpload :: error");
+
+      return false
+    }
+  }
+  async deleteFile(fileId){
+    try {
+      return await this.bucket.deleteDocument(
+        conf.appwriteBucketId,
+        fileId
+      )
+      
+    } catch (error) {
+      console.log("Appwrite service :: file delete :: error");
+
+      return false
+    }
+  }
+
+  filePreview(fileId){
+    return this.bucket.getFilePreview(
+      
+    )
+  }
+
+
 }
 
 const service = new Service({});
