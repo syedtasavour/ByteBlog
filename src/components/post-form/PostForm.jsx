2ee0e2e86928abc1b292f2643b2bcb1ead6309b7
Syed Tasavour <syedtasavour86@gmail.com>
2025-02-22 02:21:21 +0530

    refactor PostForm component to handle post updates and submissions with image upload functionality

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -5,29 +5,54 @@ import appwriteService from "../../appwrite/config";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-function PostForm({post}) {
-    const {register,handleSubmit,watch,setvalue,control,getValues} = useForm({
-        defaultValues:{
-            title: post?.title || '',
-            slug: post?.slug || '',
-            content: post?.content || '',
-            status: post?.status || 'active'
+function PostForm({ post }) {
+  const { register, handleSubmit, watch, setvalue, control, getValues } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "active",
+      },
+    });
 
-            
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user.userData);
+
+  const submit = async (data) => {
+    if (post) {
+      const file = data.image[0]
+        ? appwriteService.uplaodFile(data.image[0])
+        : null;
+      if (file) {
+        appwriteService.deleteFile(post.featuredImage);
+      }
+      const dbPost = await appwriteService.updatePost(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined,
+        if(dbPost){
+            navigate(`/post/${dbPost.$id}`)
         }
-    })
+      });
+    }else{
+        const file = data.image[0]
+        ? appwriteService.uplaodFile(data.image[0])
+        : null;
+        if(file){
+            const fileId  = file.$id 
+            data.featuredImage = fileId
+            const dbPost = await appwriteService.createPost({
+                ...data,
+                userId: userData.$id
+            }) 
+            if(dbPost){
+                navigate(`/post/${dbPost.$id}`)
+            }
+        }
+    }
+  };
 
-    const navigate = useNavigate()
-    const userData = useSelector(
-        state => state.user.userData
-    )
-    
-     
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
 
-export default PostForm
+export default PostForm;
